74bfe5253b2d419c41c939e99869d7e1a0d5d978
ArtMichel <a2019116758@estudiantes.upsa.edu.bo>
2020-09-15 01:14:59 -0400

    Se creo el modelo

---
+++ PracticaExamen1/PracticaExamen1/Models/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PracticaExamen1.Models
+{
+    public class DataContext
+    {
+    }
+}
---
+++ PracticaExamen1/PracticaExamen1/Models/Michel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PracticaExamen1.Models
+{
+    public enum CategoryType
+    {
+        Zoologico = 1,
+        Centro = 2,
+        Plaza = 3,
+        Ventura = 4,
+        Urubo = 5
+    }
+    public class Michel
+    {
+        [Key]
+        public int MichelID { get; set; }
+        [Required]
+        [Display(Name = "Nombre Completo")]
+        [StringLength(30, ErrorMessage = "El campo {0} debe contener entre {2} y {1} caracteres", MinimumLength = 2)]
+        public string FriendofMichel { get; set; }
+        [Required]
+        public CategoryType Place { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Cumpleaños")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Birthdate { get; set; }
+    }
+}
